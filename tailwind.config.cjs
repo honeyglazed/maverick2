e9f274dcde907909efa3ceb42c53a3a6b09e07fb
Michael Ho <hth.michael@gmail.com>
2023-05-22 16:44:39 -0400

    Working MVP

---
+++ tailwind.config.cjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		fontFamily: {
+			'serif': [
+				'"Cormorant Garamond"', 
+				'ui-serif', 
+				'Georgia', 'Cambria', 
+				'"Times New Roman"', 
+				'Times', 
+				'serif'
+			]
+		},
+		extend: {
+			colors: {
+				"off-white": "#f7f7f2",
+				"black": "#121212"
+			}
+		},
+	},
+  plugins: [
+    require('@tailwindcss/typography')
+  ]
+}
