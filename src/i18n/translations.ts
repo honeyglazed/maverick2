89e397442aefabf1000a6f1046b3c7238c5b8bec
Michael Ho <hth.michael@gmail.com>
2023-05-28 16:13:53 -0400

    Add contact page

--- src/i18n/translations.ts
+++ src/i18n/translations.ts
@@ -11,12 +11,22 @@ const ui = {
         'nav.about': 'About',
         'nav.brand': 'Press Release',
         'nav.contact': 'Contact',
+        'social.wechat': 'WeChat',
+        'social.ig': 'Instagram',
+        'social.fb': 'Facebook',
+        'social.red': 'XiaoHongShu',
+        'social.tb': 'TaoBao',
     },
     cn: {
         'nav.home': 'Maverick',
         'nav.about': '关于',
         'nav.brand': '品牌资源',
         'nav.contact': '联系方式',
+        'social.wechat': '微信',
+        'social.ig': 'Instagram',
+        'social.fb': 'Facebook',
+        'social.red': '小红书',
+        'social.tb': '淘宝',
     },
 };
 
