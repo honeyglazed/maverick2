67f397881ea3632dd815855003746d76fee6ae56
Michael Ho <hth.michael@gmail.com>
2023-06-10 22:22:50 -0400

    Add resources page

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,12 +3,13 @@ import { z, defineCollection } from 'astro:content';
 const resourceCollection = defineCollection({
     type: 'data',
     schema: ({image}) => z.object({
+        path: z.string(),
         title: z.string(),
         language: z.enum(['en', 'cn']),
         description: z.string(),
         thumbnail_img: image(),
-        featured_img: z.string(),
-        gallery_imgs: z.array(z.string())
+        featured_img: image(),
+        gallery_imgs: z.array(image())
     })
 })
 
